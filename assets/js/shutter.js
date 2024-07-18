22cdaad51751f11fb323727d10e86b7834953b51
Hobgoblin1123 <dedroota@gmail.com>
2024-07-18 16:11:38 +0900

    オープニングシャッター完成

--- assets/js/shutter.js
+++ assets/js/shutter.js
@@ -1,11 +1,17 @@
 jQuery(function(){
-    //shutter1がフェードアウトしてから300ミリ秒後にshutter2のアニメーション(lazer)実行
-      $('#lazer').css('animation','lazer forwards 4.0s');
+    $('#lazer').css('animation','lazer forwards 4.0s');
     //lazerアニメーションが終わりそうなときにshutter2自体をフェードアウトさせる
-      setTimeout(function(){
-        $('#shutter1').fadeOut(1000);
-      },3000);
     setTimeout(function(){
-      $('shutter#1').remove();
-    },4500);
+      $('#shutter1').fadeOut(1000);
+    },3000);
+
+    setTimeout(function(){
+      $('#s-logo').css('animation','scale-change 1.75s forwards');
+      setTimeout(function(){
+        $('#meteor').css('animation','ryusei 1s forwards');
+      },2000);
+      setTimeout(function(){
+        $('#shutter2').fadeOut(1000);
+      },4750)
+    },3000);
 });
